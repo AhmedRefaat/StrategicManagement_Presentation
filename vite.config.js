3f7a9da29ee56243aab4481726ccf02b5691ed18
ahmedrmohamed <ahmed.mohamed@bmw.de>
2026-08-28 19:11:33 +0200

    add new deploy code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  build: { outDir: 'dist', emptyOutDir: true }
+export default defineConfig(() => {
+  const repositoryName =
+    process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+
+  const isGitHubPagesBuild =
+    process.env.GITHUB_ACTIONS === "true";
+
+  const isUserOrOrganizationSite =
+    repositoryName.endsWith(".github.io");
+
+  const base = isGitHubPagesBuild
+    ? isUserOrOrganizationSite
+      ? "/"
+      : `/${repositoryName}/`
+    : "/";
+
+  return {
+    plugins: [react()],
+    base,
+
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+    },
+  };
 });
